40403feabbf091ddcd9b3465e614f10157bdcdc7
Carrie Coxwell <carrie.coxwell@gmail.com>
2020-01-26 17:01:58 -0600

    recent lists

--- weekgen.sh
+++ weekgen.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-hugo new csp/post/2020-01-21.md
-hugo new csp/post/2020-01-22.md
-hugo new csp/post/2020-01-24.md
+hugo new csp/post/2020-01-27.md
+hugo new csp/post/2020-01-28.md
+hugo new csp/post/2020-01-29.md
+hugo new csp/post/2020-01-30.md
+hugo new csp/post/2020-01-31.md
 
-hugo new csd/post/2020-01-21.md
-hugo new csd/post/2020-01-22.md
-hugo new csd/post/2020-01-24.md
+hugo new csd/post/2020-01-27.md
+hugo new csd/post/2020-01-28.md
+hugo new csd/post/2020-01-29.md
+hugo new csd/post/2020-01-30.md
+hugo new csd/post/2020-01-31.md
 
 
-hugo new csdwa/post/2020-01-21.md
-hugo new csdwa/post/2020-01-22.md
-hugo new csdwa/post/2020-01-24.md
+hugo new csdwa/post/2020-01-27.md
+hugo new csdwa/post/2020-01-28.md
+hugo new csdwa/post/2020-01-29.md
+hugo new csdwa/post/2020-01-30.md
+hugo new csdwa/post/2020-01-31.md
